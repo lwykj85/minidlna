5a14de644baa4039cbb299318bea7f3cef83811c
lwykj85 <42383341+lwykj85@users.noreply.github.com>
2020-03-05 01:01:40 +0800

    Update utils.c
    //----add----  
            ends_with(file, ".rm")  || ends_with(file, ".rmvb")  ||  
    //----end----

--- utils.c
+++ utils.c
@@ -401,6 +401,9 @@ is_video(const char * file)
 		ends_with(file, ".m2t") || ends_with(file, ".mkv")   ||
 		ends_with(file, ".vob") || ends_with(file, ".ts")    ||
 		ends_with(file, ".flv") || ends_with(file, ".xvid")  ||
+		//----add----  
+		ends_with(file, ".rm")  || ends_with(file, ".rmvb")  ||  
+		//----end----  
 #ifdef TIVO_SUPPORT
 		ends_with(file, ".TiVo") ||
 #endif
